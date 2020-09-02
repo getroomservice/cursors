597635958001bd1cecf00352f922030f6a0c22cc
Flaque <evanjamesconrad@gmail.com>
2020-09-02 15:15:17 -0700

    add some stuff

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export default async (req, res) => {
+  const body = JSON.parse(req.body);
+  const API_KEY = "nEK9OXZsk5G0gdEGieqwy";
+  const user = "some-user-" + getRandomInt(1, 200);
+
+  const r = await fetch("https://super.roomservice.dev/provision", {
+    method: "post",
+    headers: {
+      Authorization: `Bearer: ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      user: user,
+      resources: body.resources,
+    }),
+  });
+
+  const json = await r.json();
+  console.log(json);
+
+  res.json(json);
+};
